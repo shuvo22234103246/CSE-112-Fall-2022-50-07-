cff2d3a189514271590500ed732e189083baa48f
shuvo chakma <122222782+shuvo22234103246@users.noreply.github.com>
2023-03-27 23:06:58 +0600

    Add files via upload

---
+++ Lab-11/Anton_and_danik.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n,a=0,d=0;
+    cin>>n;
+    string s;
+    cin>>s;
+for(int i=0;i<s.size();i++)
+    {
+    if(s[i]=='A')
+    {
+       a++;
+    }
+       else
+       {
+         d++;
+
+       }
+
+    }
+
+
+if(a>d)
+{
+
+    cout<<"Anton";
+}
+
+else if(a==d)
+{
+    cout<<"Friendship";
+}
+
+else
+    cout<<"Danik";
+
+
+return 0;
+
+}
